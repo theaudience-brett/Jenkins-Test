251dee8144ba38c78b7cdcd4d69a2b8d22594492
Brett Mack <brett@theaudience.com>
2012-02-10 03:31:28 -0800

    Made a change to throw off the results

--- src/controller/BankAccountController.php
+++ src/controller/BankAccountController.php
@@ -37,4 +37,30 @@ class BankAccountController implements Controller
 
         return 'BankAccountView';
     }
+
+    public function executeSecond(Request $request, Response $response)
+    {
+        try {
+            $id = $request->get('id');
+        }
+
+        catch (OutOfBoundsException $e) {
+            throw new ControllerException('No bank account was specified.');
+        }
+
+        try {
+            $ba = $this->mapper->findById($id);
+        }
+
+        catch (OutOfBoundsException $e) {
+            throw new ControllerException(
+              sprintf('No bank account with id #%d exists.', $id)
+            );
+        }
+
+        $response->set('id', $id);
+        $response->set('balance', $ba->getBalance());
+
+        return 'BankAccountView';
+    }
 }
