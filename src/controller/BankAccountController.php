408c782357397a2f70d5e4766d00d2acf5245247
Brett Mack <brett@theaudience.com>
2012-02-10 03:42:54 -0800

    Created a code error to test lint

--- src/controller/BankAccountController.php
+++ src/controller/BankAccountController.php
@@ -15,7 +15,7 @@ class BankAccountController implements Controller
     public function execute(Request $request, Response $response)
     {
         try {
-            $id = $request->get('id');
+            $id = $request->get('id')
         }
 
         catch (OutOfBoundsException $e) {
@@ -38,29 +38,4 @@ class BankAccountController implements Controller
         return 'BankAccountView';
     }
 
-    public function executeSecond(Request $request, Response $response)
-    {
-        try {
-            $id = $request->get('id');
-        }
-
-        catch (OutOfBoundsException $e) {
-            throw new ControllerException('No bank account was specified.');
-        }
-
-        try {
-            $ba = $this->mapper->findById($id);
-        }
-
-        catch (OutOfBoundsException $e) {
-            throw new ControllerException(
-              sprintf('No bank account with id #%d exists.', $id)
-            );
-        }
-
-        $response->set('id', $id);
-        $response->set('balance', $ba->getBalance());
-
-        return 'BankAccountView';
-    }
 }
